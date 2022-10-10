15fc6bc1fde1e2ff2ecd336413ea41572b4e14ae
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-10-10 13:22:36 +0300

    Add files via upload

---
+++ George Python 1 töö.py
@@ -0,0 +1,61 @@
+#George Raatpalu
+#10.10.22
+#harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(400,400)
+
+#taavi = Turtle.Turtle()
+#taavi.forward(150)
+#taavi.left(90)
+#taavi.fd(100) #forward
+#taavi.fd(100)#forward
+#taavi.It(90) #left
+#taavi.fd(90)
+#taavi.it(90)
+#taavi.fd(90)
+#"suleb akna klikkides"
+
+k1 = turtle.Turtle()
+k2 = turtle.Turtle()
+k3 = turtle.Turtle()
+k4 = turtle.Turtle()
+k5 = turtle.Turtle()
+k6 = turtle.Turtle()
+k7 = turtle.Turtle()
+k8 = turtle.Turtle()
+
+k1.color("lime")
+k2.color("green")
+k3.color("blue")
+k4.color("black")
+k5.color("red")
+k6.color("purple")
+k7.color("orange")
+k8.color("blue")
+
+k1.fd(90)
+k2.left(45)
+k2.fd(90)
+k3.left(90)
+k3.forward(90)
+k4.left(135)
+k4.fd(90)
+k5.left(180)
+k5.fd(90)
+k6.lt(225)
+k6.fd(90)
+k7.lt(270)
+k7.fd(90)
+k8.lt(315)
+k8.fd(90)
+
+
+#suleb akna klikkides
+turtle.exitonclick()
+
+
+
+
