f81609f8fbe66c93f5b928c7eb4ad547f6b1ef36
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-10-10 14:07:29 +0300

    Add files via upload

---
+++ Python 3 töö george Raatpalu.py
@@ -0,0 +1,25 @@
+#George Raatpalu
+#10.10.22
+#harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(400,400)
+aken.title("Ülesanne 2 - GR")
+
+
+k1 = turtle.Turtle()
+for i in range(4):
+    k1.fd(100)
+    k1.lt(180)
+    k1.lt(90)
+    k1.lt(180)
+    
+    
+    
+    
+    
+
+
+
