1616363364866ede10e40479ee6678befaf04d58
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-10-17 14:04:07 +0300

    Add files via upload

---
+++ python juubel.py
@@ -0,0 +1,16 @@
+#juubel
+sp = "09.08.2006"
+aasta =  2022
+p,k,a =sp.split(".")
+vanus = aasta-int(a)
+
+
+  
+ 
+if vanus%5==0:
+    print("juubel")
+else:
+    print("print ei ole juubel")
+
+ 
+ 
