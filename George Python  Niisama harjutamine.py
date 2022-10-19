4b5f591d8a7c04a5877b87c5ae96e4020242b14c
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-10-19 19:36:11 +0300

    Add files via upload

---
+++ George Python  Niisama harjutamine.py
@@ -0,0 +1,39 @@
+#George Raatpalu
+#11.10.22
+#harjutus01
+
+#niisama harjutamine
+s1 = int(input("lisa arv1: "))
+s2 = int(input("lisa arv2: "))
+print(s1+s2)
+
+
+
+
+
+enimi = "mari"
+pnimi = "kisakõri"
+enimi = "kadri"
+nimi  = enimi+" "+pni
+print(enimi,pnimi,"ja",nimi)
+vanus = 18 #int
+pikkus = 1.52 #float
+summa = vanus*pikkus # +-*/ //
+vastus = 5//2 #täisarvuline jagamine
+vastus = 5%2 #jääk
+print(vastus)
+
+
+print("tekst")
+print('tekst')
+print("it's my \"life\"")
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print(' @..@')
+print('(---)')
+print('( \_/ )')
+print('^^ "" ^^')
+
+a = input("sisesta ruudu külje pikkus:")
+s = a*a
+print("Vastus: ruudu pindala on" ,s, "cm^2")
