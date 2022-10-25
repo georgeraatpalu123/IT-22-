19de6641cdf21afac8c1173935a2bb5be6f7fde5
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-10-25 10:06:11 +0300

    Add files via upload

---
+++ Python ül 5 loend george.py
@@ -0,0 +1,33 @@
+#George Raatpalu
+#Harjutus05
+#25.10.22
+
+#Nimede lisamine loendisse
+#nimed = []
+#for i in range(5):
+    #nimi = input("Lisa nimi loendisse: ")
+   # nimed.append(nimi)
+#nimed.sort()
+#print(nimed)
+#print(f"Viimati lisatud nimi:{nimed[-1]}")
+
+
+
+
+
+#Õpilased
+jrk = 1
+opilased=["Juhan","Kati","Maarja","Mario","Mati"]
+for i in opilased:
+    print(f"{jrk}. {i}")
+    jrk+=1
+num=int(input("mitmendat tahad muuta: "))
+opilane=input("milleks tahad muuta: ")
+
+print(opilased)
+
+
+
+
+
+
