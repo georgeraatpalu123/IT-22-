358e3e0626f6f9e67fdbb7c27ba557098375e280
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-10-26 11:05:03 +0300

    Add files via upload

---
+++ Python 5.py
@@ -0,0 +1,15 @@
+
+
+#Duplikaadid
+#opilased=["Juhan","Kati","Mario","Mario","Mati","Mati"]
+#opilased.remove("Mario")
+#opilased.remove("Mati")
+#print(opilased)
+
+
+#Vanused
+vanus=(1,11,23,45,56,75)
+list1=[1,11,23,45,56,75]
+
+
+print(vanus)
