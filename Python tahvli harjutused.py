e1fb561bd514d02a6f8ef2e9578bed4d3452d3c7
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-11-02 10:03:11 +0200

    Add files via upload

---
+++ Python tahvli harjutused.py
@@ -0,0 +1,38 @@
+#George Raatpalu
+#Python is.töö 1
+#02.11.22
+
+#tervituss
+print("Tere, maailm")
+
+#Aasta liblikas
+aasta = 2020
+liblikas = ("teelehe-moosaiikliblikas")
+lause_keskosa = ("aasta liblikas on")
+lause=str(aasta)+lause_keskosa+liblikas
+print(lause)
+
+#Pilved
+
+a = input("pilvede alus")
+print(a)
+
+
+#Bussid
+input("inimeste arv")
+kohad = int(input("Kohtade arv: "))
+busse_vaja = (inimesed/kohad)
+print(f"Busse on vaja {math.coil(busse_vaja)}")
+jaak = inimesed%kohad
+if jaak==0:
+    print("viimases bussis on inimesi {kohad}")
+else: print("viimases bussis on inimesi {jaak}")
+
+
+
+    
+
+
+
+
+          
