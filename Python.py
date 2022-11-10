36a6b53a5510e38f45bc3726a165d50f9bd5fb17
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-11-10 09:56:22 +0200

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,37 @@
+#Jänesed      2.1, 2,2, 2,5
+
+#Porgandid=2
+#a=int(input("Sisestage ringide arv "))
+#i = 0
+#while i <a:
+    #i += 1
+    
+#Porgandid*=6
+#print(Porgandid)
+
+
+
+#Sissetulekud
+
+#f = open("konto.txt")
+#for line in f
+    #if re.search("^[0-9]",-line):
+         #print(line,end="")
+        
+        
+        
+#Jukebox
+x=input("Millist mussi tahate? " )
+file=open(x)
+for rida in file:
+    print(rida)
+
+
+
+
+
+
+
+
+
+
