79e7a8fad4e3a4a8e1ab9b3902c7397a36ac7dff
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-11-30 11:46:11 +0200

    Add files via upload

---
+++ Windows PowerShell ISE Ülesanne 05.ps123.ps1
@@ -0,0 +1,16 @@
+﻿$a=hostname
+"Arvuti nimi: "+ $a
+
+(Get-WmiObject Win32_OperatingSystem)
+"***********************************"
+(Get-WmiObject Win32_OperatingSystem).Caption
+"***********************************"
+(Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Ethernet).IPAddress
+"***********************************"
+(get-wmiobject -class "win32_physicalmemory" -namespace "root\CIMV2").Capacity
+"***********************************"
+(Get-WMIObject win32_Processor).Name
+"***********************************"
+(Get-WmiObject Win32_VideoController).Name
+"***********************************"
+[System.Security.Principal.WindowsIdentity]::GetCurrent().Name
