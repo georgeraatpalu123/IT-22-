f25b46ae7f7c8cd6e0d209486025915ca75b19fd
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-12-05 13:20:38 +0200

    Add files via upload

---
+++ George Raatpalu Powershell 06.ps1
@@ -0,0 +1,22 @@
+﻿$nimi = "Georgeraatpalu"
+$email = "georgeraatpalu10@gmail.com"
+$kuupaev = get-date -Format "dd.mm.yy"
+
+
+$nimi
+$email
+$kuupaev
+
+$skriptiAsukoht = $MyInvocation.MyCommand.Path
+$dir = Split-Path $skriptiAsukoht
+$emailid = Get-Content -path $dir\emailid.txt
+$file_data | Select-Object -Last 1
+
+
+$kasutajad=$emailid.split(",")
+$kasutajad+=$email
+"esimene kasutaja on "+$kasutajad[0]
+"viimane kasutaja on "+$kasutajad[-1]
+"kasutajaid kokku " +$emailid.Length
+
+
