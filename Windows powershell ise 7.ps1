6331fa4bdbafae13fd49a37fb54c078c2cfecee8
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-12-06 09:05:16 +0200

    Add files via upload

---
+++ Windows powershell ise 7.ps1
@@ -0,0 +1,5 @@
+﻿iex hostname
+Get-PhysicalDisk
+Get-PSDrive
+Get-WmiObject win32_logicaldisk
+Get-CimInstance -Class CIM_LogicalDisk | Select-Object @{Name="Size(GB)";Expression={$_.size/1gb}}, @{Name="Free Space(GB)";Expression={$_.freespace/1gb}}, @{Name="Free (%)";Expression={"{0,6:P0}" -f(($_.freespace/1gb) / ($_.size/1gb))}}, DeviceID, DriveType | Where-Object DriveType -EQ '3'
