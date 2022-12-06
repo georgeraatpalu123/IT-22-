70d9b7305c2cf753ce808b222569b59c4826f43b
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-12-06 10:02:01 +0200

    Add files via upload

---
+++ Windows powershell 08.ps1
@@ -0,0 +1,24 @@
+﻿function Calculate-Circle {
+    param(
+        [float]$radius
+    )
+
+    $surface = $radius*$radius*[Math]::PI
+
+     $surface
+}
+Calculate-Circle(15)
+
+
+
+
+function puhasta_nimi {
+
+param($nimi)
+$nimi=$nimi.Trim()
+$nimi=$nimi.Replace("ü","u").Replace("õ","o").Replace("ä","a").Replace("ö","o")
+$nimi
+}
+
+
+puhasta_nimi("  Jüõäöri  ")
