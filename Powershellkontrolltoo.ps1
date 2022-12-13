2e8679beeb7d45a80861395362b7a659cce3519b
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-12-13 11:43:42 +0200

    Add files via upload

---
+++ Powershellkontrolltoo.ps1
@@ -0,0 +1,25 @@
+﻿$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+ 
+
+#$csv_header = @("Nimed;Kasutajanimi;email;password")
+ 
+
+#$csv_header | Set-Content $dir\emails.csv
+ 
+$users = Get-Content -path C:\Users\It22\Desktop\xd\emails.csv
+ 
+ $users
+
+
+
+
+ForEach($user in $users){
+    #$fname = $user.first_name
+    #$lname = $user.last_name
+    $user
+    $users
+    }
+      
+$pass += Get-Random  -minimum 100 -Maximum 999
+
