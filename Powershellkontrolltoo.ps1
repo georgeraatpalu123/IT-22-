9924c157d2d9e708a3a64cb6035a45da9ef16496
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2022-12-14 10:02:34 +0200

    Add files via upload

--- Powershellkontrolltoo.ps1
+++ Powershellkontrolltoo.ps1
@@ -1,4 +1,7 @@
-﻿$scriptpath = $MyInvocation.MyCommand.Path
+﻿#George Raatpalu
+#13.12.2022
+
+$scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
  
 
@@ -6,20 +9,25 @@ $dir = Split-Path $scriptpath
  
 
 #$csv_header | Set-Content $dir\emails.csv
- 
+#massiivid
 $users = Get-Content -path C:\Users\It22\Desktop\xd\emails.csv
  
- $users
+$users
 
 
 
 
 ForEach($user in $users){
-    #$fname = $user.first_name
-    #$lname = $user.last_name
+    $fname = $user.first_name
+    $lname = $user.last_name
     $user
     $users
-    }
-      
+    $fullname = $fname+$lname
+}      
+$pass = 0 | ForEach-Object { Get-Random -Maximum $lname.Length }
+$pass = -join $lname[$pass]
 $pass += Get-Random  -minimum 100 -Maximum 999
+$items = Get-ChildItem -Path "c:\temp"
+
+$fullname >> C:\Users\It22\Desktop\xd/emails.txt
 
