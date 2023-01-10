f53763e5efe21488e3a493b500ba76f02a9fbf18
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2023-01-10 10:38:41 +0200

    Add files via upload

---
+++ Tkinter 3.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+aken = Tk()
+
+
+Label(aken, text="NIMI: George Raatpalu", foreground="red",background="black", font="Tahoma 16 bold italic").pack()
+Label(aken, text="Rühm: IT22", foreground="red",background="black", font="Tahoma 16 bold italic").pack()
+Label(aken, text="2023", foreground="red",background="black", font="Tahoma 16 bold italic").pack()
+
+aken["background"]='black'
+
+
+
+aken.mainloop()
